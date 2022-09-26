e2f93b3fa8637887ac980cb78877b9ab85b3577f
Baptiste Foezon <baptiste.foezon@gmail.com>
2022-09-26 17:32:21 +0200

    interface display

---
+++ keysafe_client/src/display.rs
@@ -0,0 +1,80 @@
+use std::io;
+pub trait Interface {
+    fn display_menu(&self);
+    fn create_acount(&self) -> user;
+}
+
+pub struct Terminal_Interface{
+
+}
+
+pub struct GUI_Interface{
+
+}
+
+
+impl Interface for Terminal_Interface{
+    fn display_menu(&self) {
+        println!("Bienvenue dans keysafe !!");
+        println!("1. Créer un compte");
+        println!("2. Se connecter ");
+    }
+
+    fn  create_acount(&self) ->  user{
+
+        let mut nom = String::new();
+        let mut prenom = String::new();
+        let mut mail = String::new();
+        let mut mdp1 = String::new();
+        let mut mdp2 = String::new();
+        println!("Création de votre compte :");
+        println!(". Nom ");
+        io::stdin().read_line(&mut nom).expect("Veuillez rentrez une valeur correct");
+        //f.write_all(b"Hello, world! ")?;
+        println!(". Prénom ");
+        io::stdin().read_line(&mut prenom).expect("Veuillez rentrez une valeur correct");
+        println!(". Email ");
+        io::stdin().read_line(&mut mail).expect("Veuillez rentrez une valeur correct");
+        println!(". mdp ");
+        io::stdin().read_line(&mut mdp1).expect("Veuillez rentrez une valeur correct");
+        println!(". mdp ");
+        io::stdin().read_line(&mut mdp2).expect("Veuillez rentrez une valeur correct");
+        return user::new(nom, mail, mdp1)
+
+    }
+}
+
+
+impl Interface for GUI_Interface{
+    fn display_menu(&self) {
+        println!("GUI")
+    }
+
+    fn create_acount(&self )-> user {
+        todo!()
+    }
+}
+
+pub trait User{
+    fn new(name: String, email: String, mdp : String) -> user;
+
+}
+
+pub struct user{
+    pub name : String,
+    pub email : String,
+    pub mdp : String,
+
+}
+
+impl User for user{
+    fn new(name :String , email :String, mdp : String) -> user{
+        user{
+            name,
+            email,
+            mdp
+        }
+    }
+
+}
+
