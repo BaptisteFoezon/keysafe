928f15eed3d08f1cc88c1d9a63f40e49662eea4c
Baptiste Foezon <baptiste.foezon@gmail.com>
2022-10-03 17:59:07 +0200

    debut de correction

--- keysafe_client/src/display.rs
+++ keysafe_client/src/display.rs
@@ -1,62 +1,70 @@
 use std::io;
+use std::io::Error;
+
+use crate::login::{Login, login};
 use crate::user::{User, user};
 
 pub trait Interface {
-    fn display_menu(&self);
+    fn display_menu(&self) -> Result<String, std::io::Error>;
     fn display_create_account_success();
     fn create_account(&self) -> Result<user, std::io::Error>;
-    fn sign_in(&self) ;
+    fn sign_in(&self);
+    fn user_connected(&self);
+    fn main_menu(&self);
+    fn new_password(&self) -> Result<login, std::io::Error>;
 }
 
-pub struct Terminal_Interface{}
+pub struct Terminal_Interface {}
 
-pub struct GUI_Interface{}
+pub struct GUI_Interface {}
 
 
-impl Interface for Terminal_Interface{
-    fn display_menu(&self) -> Result<u8, std::io::Error>;{
-        let mut display_menu_choice  u8;
+impl Interface for Terminal_Interface {
+    fn display_menu(&self) -> Result<String, std::io::Error> {
+        let mut display_menu_choice = String::new();
         println!("Bienvenue dans keysafe !");
         println!("1. Créer un compte");
         println!("2. Se connecter ");
         io::stdin().read_line(&mut display_menu_choice)?;
+        Ok(display_menu_choice)
     }
 
     fn display_create_account_success() {
         println!("Compte créé avec succès !");
     }
 
-    fn  create_account(&self) ->  Result<user, std::io::Error>{
-
+    fn create_account(&self) -> Result<user, std::io::Error> {
         let mut nom_in = String::new();
         let mut mail = String::new();
         let mut mdp1 = String::new();
         let mut mdp2 = String::new();
         println!("Création de votre compte...");
         println!("Pseudo :");
-        io::stdin().read_line( &mut nom_in)?;
+        io::stdin().read_line(&mut nom_in)?;
         println!("Adresse mail :");
-        io::stdin().read_line( &mut mail)?;
+        io::stdin().read_line(&mut mail)?;
         println!("Mot de passe :");
-        io::stdin().read_line( &mut mdp1)?;
+        io::stdin().read_line(&mut mdp1)?;
         println!("Mot de passe :");
-        io::stdin().read_line( &mut mdp2)?;
+        io::stdin().read_line(&mut mdp2)?;
         Ok(user::new(nom_in.trim(), mail.trim(), mdp1.trim()))
+    }
 
+    fn sign_in(&self) {
+        todo!()
     }
 
     fn user_connected(&self) {
         println!("Vous êtes désormais connecté.");
     }
 
-    fn main_menu(&self) ->  Result<user, std::io::Error>{
-        let mut main_menu_choice u8;
+    fn main_menu(&self) {
         println!("Que voulez vous faire ?");
         println!("1. Consulter mes mots de passe enregistrés");
         println!("2. Enregistrer un nouveau mot de passe");
     }
 
-    fn new_password(&self) -> Result<login, std::io::Error>{
+    fn new_password(&self) -> Result<login, std::io::Error> {
         let mut url = String::new();
         let mut mail = String::new();
         let mut pwd = String::new();
@@ -66,28 +74,9 @@ impl Interface for Terminal_Interface{
         io::stdin().read_line(&mut mail)?;
         println!("Indiquez votre mot de passe :");
         io::stdin().read_line(&mut pwd)?;
+        Ok(login::new(url.trim(), mail.trim(), pwd.trim()))
     }
 }
 
-
-
-
-impl Interface for GUI_Interface{
-    fn display_menu(&self) {
-        println!("GUI")
-    }
-
-    fn display_create_account_success() {
-        todo!()
-    }
-
-    fn create_account(&self )-> Result<user, std::io::Error> {
-        todo!()
-    }
-
-    fn sign_in(&self) {
-        todo!()
-    }
-}
 
 
--- keysafe_client/src/main.rs
+++ keysafe_client/src/main.rs
@@ -1,26 +1,28 @@
-mod display;
-mod user;
-
-use std::io;
-use std::io::Write;
+use std::fs;
 use std::fs::File;
 use std::fs::OpenOptions;
-use std::fs;
+use std::io;
+use std::io::Write;
 use std::str;
 
 use pwhash::bcrypt;
+
 use display::Interface;
+
+mod display;
+mod user;
+mod login;
 
 fn main() {
     let interface = display::Terminal_Interface {};
-    let mut choice u8 = interface.display_menu();
-    if choice == 1 {
+    let mut choice = interface.display_menu().expect(" ");
+    if choice == "1" {
         let user = interface.create_account().expect("dsqdsq");
         println!("{:?}", user);
         let mdp_hash = bcrypt::hash(user.mdp).unwrap();
         users_store(&user.pseudo, &mdp_hash).expect("Une erreur est survenue");
         println!("mot de pass hasher : {}", mdp_hash);
-        interface.display_create_account_success();
+        interface.display_create_account_success()?;
     }
 
     if choice == 1 {
@@ -29,22 +31,19 @@ fn main() {
 
     if choice == 2 {
         interface.user_connected();
-        let mut main_choice = interface.main_menu();
+        let mut main_choice = interface.main_menu()?;
         if main_choice == 2 {
-            let login = interface.new_password();   
+            let login = interface.new_password()?;
         }
     }
-
-
-
 }
 
 
 fn users_store(id: &String, main_pwd: &String) -> std::io::Result<()> {
-    let mut extension : String = ".pwd".to_owned();
-    let mut extension2 : String = ".data".to_owned();
-    let mut idToOwned : String = id.to_string();
-    let mut idToOwned2 : String = id.to_string();
+    let mut extension: String = ".pwd".to_owned();
+    let mut extension2: String = ".data".to_owned();
+    let mut idToOwned: String = id.to_string();
+    let mut idToOwned2: String = id.to_string();
     idToOwned.push_str(&extension);
     idToOwned2.push_str(&extension2);
     println!("Voici le pointeur id : {} ", &id);
