721cf8c5539218e3cb329ac1f7cf48e880a4f0cd
Baptiste Foezon <baptiste.foezon@gmail.com>
2022-10-17 10:39:08 +0200

    rename folder

---
+++ keysafe_srv/src/main.rs
@@ -0,0 +1,24 @@
+extern crate core;
+
+use display::Interface;
+use state_machine::SM;
+
+
+mod display;
+mod state_machine;
+mod user;
+
+use bouncer::BouncerTrait;
+
+//use Login::Login;
+
+mod bouncer;
+mod file_manager;
+mod login;
+mod tcp;
+
+fn main() {
+    let interface = display::TerminalInterface {};
+    let mut st = SM::new(interface);
+    st.start();
+}
