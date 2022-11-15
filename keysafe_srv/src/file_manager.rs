d17882edfe7800905601db3106e67b88372a9210
Xavier Cordonnier <xavier.cordonnier@reseau.eseo.fr>
2022-11-15 17:19:34 +0100

    Ajout fonctionnalité lecture fichier JSON

--- keysafe_srv/src/file_manager.rs
+++ keysafe_srv/src/file_manager.rs
@@ -3,19 +3,20 @@ use std::io::Write;
 use std::{fs, io};
 
 use serde_derive::{Deserialize, Serialize};
+use serde_json::{Result, Value};
 
 use crate::login::Login;
 use crate::user::User;
 
 #[derive(Deserialize, Serialize, Debug)]
-struct LoginJSON {
+pub struct LoginJSON {
     url : String,
     id : String,
     password : String
 }
 #[derive(Deserialize, Serialize, Debug)]
 pub struct ListLogin {
-     list: Vec<LoginJSON>,
+     data: Vec<LoginJSON>,
  }
 
 pub trait FileManagerTrait {
@@ -24,7 +25,7 @@ pub trait FileManagerTrait {
     fn users_store(id: String, main_pwd: String) -> io::Result<()>;
     fn data_store(user: User, login: Login) -> std::io::Result<()>;
     fn get_pwd_from_file(pseudo: &str) -> io::Result<String>;
-    fn get_data_from_file() -> ListLogin;
+    fn get_data_from_file() -> io::Result<ListLogin>;
 }
 
 pub struct FileManager {}
@@ -62,13 +63,10 @@ impl FileManagerTrait for FileManager {
         let mut id_to_owned: String = user.pseudo;
         id_to_owned.push_str(&extension);
         let mut file = OpenOptions::new().append(true).open(id_to_owned)?;
-        // file.write_all(login.url.as_bytes())?;
-        // file.write_all(login.mail.as_bytes())?;
-        // file.write_all(login.pwd.as_bytes())?;
-        let login_json = LoginJSON{url: login.url, id: login.mail, password: login.pwd};
-        let j = serde_json::to_string(&login_json)?;
-        println!("On est rentré dans datastore");
-        println!("j= {}", j);
+        //let login_json = LoginJSON{url: login.url, id: login.mail, password: login.pwd};
+        let list_login = ListLogin{url: login.url, id: login.mail, password: login.pwd};
+        let j = serde_json::to_string(&list_login)?;
+        //println!("j= {}", j);
         file.write_all(j.as_bytes())?; 
 
         Ok(())
@@ -82,11 +80,12 @@ impl FileManagerTrait for FileManager {
         Ok(result)
     }
 
-    fn get_data_from_file() -> ListLogin 
+    fn get_data_from_file() -> io::Result<ListLogin> 
     {
-            let file_content = fs::read_to_string("test.json").expect("Echec ouverture fichier");
-            let list_logs: ListLogin = serde_json::from_str(&file_content).unwrap();
-            return list_logs;
-    
+            let file_content = fs::read_to_string("xav.data").expect("Echec ouverture fichier");
+            let list_logs: ListLogin = serde_json::from_str(&file_content)?;
+            //println!("Voici la liste des mdp : {:?}", list_logs);
+            //return list_logs;  
+            Ok(list_logs)  
         }
 }
